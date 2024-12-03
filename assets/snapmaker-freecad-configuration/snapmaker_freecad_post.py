e9239f89cf09afdb09dc18cb672d76926db0282a
devbrones <jonas.cronholm@protonmail.com>
2024-12-03 17:36:15 +0100

    Fixed imports for FreeCAD post-refactor compatibility

--- assets/snapmaker-freecad-configuration/snapmaker_freecad_post.py
+++ assets/snapmaker-freecad-configuration/snapmaker_freecad_post.py
@@ -27,12 +27,13 @@ import Path
 import argparse
 import datetime
 import shlex
-from PathScripts import PostUtils
+import Path.Post.Utils as PostUtils
 from PathScripts import PathUtils
-import PathScripts.PathLog as PathLog
-import PathGeom
+import Path.Log as PathLog
+import Path.Geom as PathGeom
 import math
 import base64
+print("successfully imported FreeCAD modules")
 
 LOG_MODULE = PathLog.thisModule()
 PathLog.setLevel(PathLog.Level.ERROR, LOG_MODULE)
@@ -50,13 +51,13 @@ linuxcnc_post.export(object,"/path/to/file.ncc","")
 
 now = datetime.datetime.now()
 
-parser = argparse.ArgumentParser(prog='linuxcnc', add_help=False)
+parser = argparse.ArgumentParser(prog='snapmaker_freecad', add_help=False)
 parser.add_argument('--no-header', action='store_true', help='suppress header output')
 parser.add_argument('--no-comments', action='store_true', help='suppress comment output')
 parser.add_argument('--line-numbers', action='store_true', help='prefix with line numbers')
 parser.add_argument('--no-show-editor', action='store_true', help='don\'t pop up editor before writing output')
 parser.add_argument('--precision', default='3', help='number of digits of precision, default=3')
-parser.add_argument('--segments', default='3', help='segments in curved paths: segs/cm, default=40')
+parser.add_argument('--segments', default='20', help='segments in curved paths: segs/cm, default=40')
 parser.add_argument('--break-straight', action='store_true', help='breaks also straight paths same resolution as in curved paths')
 parser.add_argument('--preamble', help='set commands to be issued before the first command, default="G17\nG90"')
 parser.add_argument('--postamble', help='set commands to be issued after the last command, default="M05\nG17 G90\nM2"')
@@ -130,7 +131,7 @@ OUTPUT_DOUBLES = True  # if false duplicate axis values are suppressed if the sa
 COMMAND_SPACE = " "
 
 LINENR = 100  # line number starting value
-SEGMENTS_PER_CM_ARC = 40 # Numbers of segments an arc should be broken into within 1cm of arc distance
+SEGMENTS_PER_CM_ARC = 80 # Numbers of segments an arc should be broken into within 1cm of arc distance
 BREAK_STRAIGHTS = False # When True, straight lines will be broken into subsegments as curved paths
 
 # These globals will be reflected in the Machine configuration of the project
@@ -138,7 +139,7 @@ UNITS = "G21"  # G21 for metric, G20 for us standard
 UNIT_SPEED_FORMAT = 'mm/min'
 UNIT_FORMAT = 'mm'
 
-MACHINE_NAME = "LinuxCNC"
+MACHINE_NAME = "Snapmaker"
 CORNER_MIN = {'x': 0, 'y': 0, 'z': 0}
 CORNER_MAX = {'x': 500, 'y': 300, 'z': 300}
 PRECISION = 3
@@ -204,7 +205,7 @@ def processArguments(argstring):
         PRECISION = args.precision
 
         SEGMENTS_PER_CM_ARC = min(max(float(args.segments), 1), 100)
-        print(SEGMENTS_PER_CM_ARC)
+        print("SEG/ARC:: "+ str(SEGMENTS_PER_CM_ARC))
         if args.preamble is not None:
             PREAMBLE = args.preamble
         if args.postamble is not None:
